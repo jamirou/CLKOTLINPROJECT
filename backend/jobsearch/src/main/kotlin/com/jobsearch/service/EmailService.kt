ff223b608f295478f0c4bc371e941ac5abb0ba32
s0alken <ropedent@gmail.com>
2024-03-31 02:39:46 -0300

    email template test (work in progress)

--- backend/jobsearch/src/main/kotlin/com/jobsearch/service/EmailService.kt
+++ backend/jobsearch/src/main/kotlin/com/jobsearch/service/EmailService.kt
@@ -6,18 +6,28 @@ import org.springframework.mail.MailException
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
+import org.thymeleaf.context.Context
+import org.thymeleaf.spring6.SpringTemplateEngine
 
 @Service
-class EmailService(private val mailSender: JavaMailSender) {
+class EmailService(private val mailSender: JavaMailSender, private val templateEngine: SpringTemplateEngine) {
 
     @Throws(MailException::class, MessagingException::class)
-    fun sendEmail(to: String, subject: String, content: String) {
+    fun sendEmail(to: String, subject: String, content: String, name: String = "Karl Jensen") {
+        val context = Context()
+
+        context.setVariable("name", name)
+        context.setVariable("sender", to)
+        context.setVariable("content", content)
+
+        val processedHtml = templateEngine.process("emailTemplate", context)
         try {
             val mimeMessage: MimeMessage = mailSender.createMimeMessage()
             val helper = MimeMessageHelper(mimeMessage, false, "utf-8")
             helper.setTo(to)
             helper.setSubject(subject)
-            helper.setText(content, true)
+            helper.setText(processedHtml, true)
+
             mailSender.send(mimeMessage)
             println("Email sent successfully to $to")
         } catch (ex: Exception) {
